27e7cc8f9e4da7ddd21f63065103144588dee6a8
pooria <106138356+P09102666127AA@users.noreply.github.com>
2024-01-07 20:31:20 +0330

    add project

---
+++ pages/index.vue
@@ -0,0 +1,208 @@
+<template>
+  <div>
+    <img src="background-option.jpg" alt="" style="width: 100%;">
+    <div class="option-position is-hidden-mobile">
+      <div class="columns mx-6 mb-0 is-justify-content-center is-multiline">
+        <div class="column is-3-desktop">
+          <a href="#">
+            <div class="position-image-suggested">
+              <div class="image-box" style="height: 550px;">
+                <img src="7.jpg" class="shadow">
+              </div>
+              <p class="title-on-picture-suggested has-text-weight-bold has-text-white">
+                زیتون اصیل خارجی و خوش طعم لوراس
+              </p>
+            </div>
+          </a>
+        </div>
+        <div class="column is-5-desktop">
+          <a href="#">
+            <div class="position-image-suggested">
+              <div class="image-box" style="height: 200px;">
+                <img src="1.jpg" class="shadow">
+              </div>
+              <p class="title-on-picture-suggested has-text-weight-bold has-text-white">
+                زیتون اصیل خارجی و خوش طعم لوراس
+              </p>
+            </div>
+          </a>
+          <div class="columns mt-0">
+            <div class="column">
+              <a href="#">
+                <div class="position-image-suggested">
+                  <div class="image-box">
+                    <img src="2.jpg" class="shadow">
+                  </div>
+                  <p class="title-on-picture-suggested has-text-weight-bold has-text-white">
+                    زیتون اصیل خارجی و خوش طعم لوراس
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="#">
+                <div class="position-image-suggested">
+                  <div class="image-box">
+                    <img src="10.jpg" class="shadow">
+                  </div>
+                  <p class="title-on-picture-suggested has-text-weight-bold has-text-white">
+                    زیتون اصیل خارجی و خوش طعم لوراس
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns mt-6 mx-4">
+      <div class="column">
+        <div class="menu-divisions pr-4">
+          <p class="has-text-weight-bold ">
+            پربازدید ترین ها
+          </p>
+        </div>
+          <div class="columns is-centered mt-2">
+            <div class="column is-three-quarters">
+              <a href="#">
+                <div class="position-image-suggested">
+                  <div class="image-box">
+                    <img src="10.jpg" class="shadow">
+                  </div>
+                  <p class="title-on-picture-popular is-size-5 has-text-white has-text-weight-bold">
+                    زیتون سبز اصیل ترکیه
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="columns is-centered">
+            <div class="column is-three-quarters">
+              <a href="#">
+                <div class="position-image-suggested">
+                  <div class="image-box">
+                    <img src="6.jpg" class="shadow">
+                  </div>
+                  <p class="title-on-picture-popular is-size-5 has-text-white has-text-weight-bold">
+                    روغن زیتون ایتالیا
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="columns is-centered">
+            <div class="column is-three-quarters">
+              <a href="#">
+                <div class="position-image-suggested">
+                  <div class="image-box">
+                    <img src="2.jpg" class="shadow">
+                  </div>
+                  <p class="title-on-picture-popular is-size-5 has-text-white has-text-weight-bold">
+                    زیتون شکسته لوراس
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="menu-divisions pr-4">
+          <p class="has-text-weight-bold ">
+           جدیدترین ها
+          </p>
+        </div>
+        <div class="columns is-centered mt-2">
+            <div class="column is-three-quarters">
+              <a href="#">
+                <div class="position-image-suggested">
+                  <div class="image-box">
+                    <img src="1.jpg" class="shadow">
+                  </div>
+                  <p class="title-on-picture-popular is-size-5 has-text-white has-text-weight-bold">
+                    زیتون شکسته ایکرام
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="columns is-centered mt-2">
+            <div class="column is-three-quarters">
+              <a href="#">
+                <div class="position-image-suggested">
+                  <div class="image-box">
+                    <img src="1.jpg" class="shadow">
+                  </div>
+                  <p class="title-on-picture-popular is-size-5 has-text-white has-text-weight-bold">
+                    زیتون شکسته ایکرام
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+      </div>
+      <div class="column has-background-light is-three-fifths">
+        <div class="menu-divisions pr-4 mr-5 my-6">
+          <p class="has-text-weight-bold is-size-5">
+            آخرین مطلب
+          </p>
+        </div>
+        <div class="columns is-centered is-mobile">
+          <div class="column">
+            <p class="has-text-weight-bold">
+              بهترین روغن زیتون ایتالیا
+            </p>
+            <p class="line-height-description mt-5 is-hidden-mobile">
+              روغن زیتون ایتالیکو محصول ایتالیا است که با بهترین روش تولید می شوند و...
+            </p>
+          </div>
+          <div class="column has-text-centered">
+            <img src="5.jpg" alt="">
+          </div>
+        </div>
+        <hr class="color-hr">
+        <div class="columns is-centered is-mobile">
+          <div class="column">
+            <p class="has-text-weight-bold">
+              اصیل ترین سرکه ایتالیا 
+            </p>
+            <p class="line-height-description mt-5 is-hidden-mobile">
+              سرکه لوگلیو که یکی از بهترین سرکه های تولید شده توسط کشور ایتالیا است...
+            </p>
+          </div>
+          <div class="column has-text-centered">
+            <img src="17.jpg" alt="">
+          </div>
+        </div>
+        <hr class="color-hr">
+        <div class="columns is-centered is-mobile">
+          <div class="column">
+            <p class="has-text-weight-bold">
+              زیتون های بدون هسته لوراس با طعم های مختلف
+            </p>
+            <p class="line-height-description mt-5 is-hidden-mobile">
+              زیتون های دانه درشت با طعم های مختلف که توسط شرکت لوراس تولید می شوند...
+            </p>
+          </div>
+          <div class="column has-text-centered">
+            <img src="18.jpg" alt="">
+          </div>
+        </div>
+        <hr class="color-hr">
+        <div class="columns is-centered is-mobile">
+          <div class="column">
+            <p class="has-text-weight-bold">
+              زیتون های کبابی لوراس بهترین زیتون های ترکیه
+            </p>
+            <p class="line-height-description mt-5 is-hidden-mobile">
+              زیتون های کبابی لوراس یکی از بهترین زیتون کبابی ها اصیل کشور ترکیه محسوب می شود...
+            </p>
+          </div>
+          <div class="column has-text-centered">
+            <img src="13.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
